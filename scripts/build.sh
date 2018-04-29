db2645f3ff77513753b8af5f84213f8b3371d142
Anh Pham <duyanhpham1509@gmail.com>
2018-04-29 14:48:51 +0300

    fix: build stage.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,10 @@ DOCKER_TAG=`git rev-parse HEAD`
 KEY_FILENAME="gcr-service-key.json"
 
 # Build binaries
-CGO_ENABLED=0 GOOS=linux go build -installsuffix cgo -o api cmd/api/main.go
-CGO_ENABLED=0 GOOS=linux go build -installsuffix cgo -o cms cmd/cms/main.go
-CGO_ENABLED=0 GOOS=linux go build -installsuffix cgo -o sap cmd/sap/main.go
+parallel -k \
+  CGO_ENABLED=0 GOOS=linux go build -installsuffix cgo -o api cmd/api/main.go \
+  CGO_ENABLED=0 GOOS=linux go build -installsuffix cgo -o cms cmd/cms/main.go \
+  CGO_ENABLED=0 GOOS=linux go build -installsuffix cgo -o sap cmd/sap/main.go
 # CGO_ENABLED=0 GOOS=linux go build -installsuffix cgo -o api cmd/frontend/main.go
 
 # Build Docker images
@@ -15,16 +16,16 @@ docker build . \
   --file cmd/api/Dockerfile
 
 docker build . \
-  --tag ${DOCKER_REGISTRY}/${PROJECT}/frontend:${DOCKER_TAG} \
-  --file cmd/frontend/Dockerfile
-
-docker build . \
   --tag ${DOCKER_REGISTRY}/${PROJECT}/cms:${DOCKER_TAG} \
   --file cmd/cms/Dockerfile
 
 docker build . \
   --tag ${DOCKER_REGISTRY}/${PROJECT}/sap:${DOCKER_TAG} \
   --file cmd/sap/Dockerfile
+
+# docker build . \
+#   --tag ${DOCKER_REGISTRY}/${PROJECT}/frontend:${DOCKER_TAG} \
+#   --file cmd/frontend/Dockerfile
 
 # Get authentication key to file
 echo ${CIRCLE_GCR_KEY} > ${HOME}/${KEY_FILENAME}
